136a75c745a642f3a97bf6c775ed2f11ab86fdc3
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-03-27 18:13:09 +0530

    connected firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,8 @@ tasks.register<Delete>("clean") {
 plugins{
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
+buildscript {
+    dependencies {
+        classpath 'com.google.gms:google-services:4.4.0'
+    }
+}
