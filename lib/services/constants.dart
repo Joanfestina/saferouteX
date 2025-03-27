96678bfb2e3ae7e0666b5a930f2581162eefc7f2
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-03-27 18:38:36 +0530

    fixed the map error

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -1,1 +1,6 @@
-const String ORS_API_KEY = "5b3ce3597851110001cf6248e6f0d7a9445048aaae45fdae37beacee";
+import 'dart:io';
+
+const String ORS_API_KEY = String.fromEnvironment(
+  'ORS_API_KEY',
+  defaultValue: "5b3ce3597851110001cf6248e6f0d7a9445048aaae45fdae37beacee", // Replace with a placeholder or remove for production
+);
